c111231feeb3ee5a71bcd65ddbcf502407d91357
Saurabh Yadav <50881320+CodeWithSaurabhYadav@users.noreply.github.com>
2023-03-13 10:37:31 +0530

    Update App.jsx
    Hiding Testimonials sectio

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,6 @@ const App = () => {
                 <Experience />
                 <Tech />
                 <Works />
-                /* <Feedbacks /> */
                 <div className="relative z-0">
                     <Contact />
                     <StarsCanvas />
