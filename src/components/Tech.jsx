ed39f1330d05d57f79510c66b0718d91fe28ea45
saurabh <50881320+CodeWithSaurabhYadav@users.noreply.github.com>
2023-04-03 20:28:50 +0530

    Saving the layout

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,22 +1,19 @@
-import { BallCanvas } from "./canvas"
-import { SectionWrapper } from "../hoc"
-import { technologies } from "../constants"
+import React from "react";
+
+import { BallCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { technologies } from "../constants";
 
 const Tech = () => {
     return (
-        <div
-            className="flex flex-row flex-wrap justify-center gap-10"
-        >
+        <div className='flex flex-row flex-wrap justify-center gap-10'>
             {technologies.map((technology) => (
-                <div 
-                    className="w-28 h-28"
-                    key={technology.name}
-                >
-                    <BallCanvas icon={technology.icon} name={technology.name}/>
+                <div className='w-28 h-28' key={technology.name}>
+                    <BallCanvas icon={technology.icon} />
                 </div>
             ))}
         </div>
-    )
-}
+    );
+};
 
-export default SectionWrapper(Tech, "")
+export default SectionWrapper(Tech, "");
